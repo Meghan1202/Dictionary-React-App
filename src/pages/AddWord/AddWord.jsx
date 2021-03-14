812cfc35f7335bd6a61b6f96041aed68f03a3e0e
Meghana Khuntia <meghanakhuntia12@gmail.com>
2021-03-14 12:28:54 +0530

    feat: add form for getting the word and sentence

--- src/pages/AddWord/AddWord.jsx
+++ src/pages/AddWord/AddWord.jsx
@@ -1,5 +1,31 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
 
-const AddWord = () => (<div>Add Word Page</div>);
+const AddWord = () => {
+  const [word, setWord] = useState('');
+  const [meaning, setMeaning] = useState('');
+
+  const handleWord = (event) => {
+    setWord(event.target.value);
+  };
+
+  const handleSentence = (event) => {
+    setMeaning(event.target.value);
+  };
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="word">What is the word?</label>
+        <br />
+        <input type="text" id="word" name="word" onChange={handleWord} value={word} />
+        <br />
+        <label htmlFor="meaning">let&apos;s use it in a sentence</label>
+        <br />
+        <input type="text" id="meaning" name="meaning" onChange={handleSentence} value={meaning} />
+      </form>
+    </div>
+  );
+};
 
 export default AddWord;
